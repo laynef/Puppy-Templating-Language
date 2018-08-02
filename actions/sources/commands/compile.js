304ba98f08d7f680f1be983b851321d0272579fa
Layne Faler <laynefaler@gmail.com>
2018-08-01 21:01:04 -0700

    BUilt templating language called Puppy that parses .puppy files

--- actions/sources/commands/compile.js
+++ actions/sources/commands/compile.js
@@ -1,19 +1,17 @@
 const fs = require('fs');
-const path = require('path');
 
 const description = 'Compile .puppy files into an html string';
 
 const command = (puppyFilePath, options) => {
     const puppyFileString = fs.readFileSync(puppyFilePath, { encoding: 'utf8' });
-    console.log(puppyFileString);
     const keywords = {
         'doctype': '<!DOCTYPE html>',
         'doctype5': '<!DOCTYPE html5>',
     };
-    const puppyNewLines = puppyFileString.split('\n').filter(e => !!e);
+    const puppyNewLines = puppyFileString.split('\n');
     const fourSpaces = new RegExp(' {4}', 'g');
-    const beginningAttributes = new RegExp('\\(', 'g');
-    const endingAttributes = new RegExp('\\)', 'g');
+    const beginningAttributes = /\(/g;
+    const endingAttributes = /\)/g;
     const beginningHtml = new RegExp('<', 'g');
     const endingHtml = new RegExp('>', 'g');
     const equalHtml = new RegExp('=', 'g');
@@ -26,62 +24,60 @@ const command = (puppyFilePath, options) => {
         if (keywords[keyword]) {
             keyword = keywords[keyword];
             array.push({ keyword });
-        }
-        if (fourSpaces.test(keyword)) {
-            keyword = keyword.replace(fourSpaces, '');
-            child = true;
-            levels = keyword.split('    ').length - 1;
-        }
-        if (beginningAttributes.test(keyword) && endingAttributes.test(keyword)) {
-            const attributes = keyword.split('(')[0].split(')')[0].split(',').reduce((acculum, item) => {
-                const keyVals = item.split('=');
-                const key = keyVals[0].replace(fourSpaces, '');
+        } else {
+            if (fourSpaces.test(keyword)) {
+                levels = keyword.split('    ').length - 1;
+                keyword = keyword.replace(fourSpaces, '');
+                child = true;
+            }
+            if (beginningAttributes.test(keyword) && endingAttributes.test(keyword)) {
+                const part = keyword.split('(');
+                const end = keyword.split(')');
+                keyword = part[0] + end[1];
+                const attributes = part[1].split(')')[0].split(',').reduce((acculum, item) => {
+                    const key = item.replace(/\'/g, '"');
+                    acculum += ` ${key}`;
+                    return acculum;
+                }, '');
+                object = { ...object, attributes };
+            }
+            if (keyword.split('=').length > 1) {
+                const equals = keyword.split('=');
+                const key = equals[0].replace(fourSpaces, '');
                 keyword = key;
-                const val = keyVals[1];
-                acculum += ` ${key}="${val}"`;
-                return acculum;
-            }, '');
-            object = { ...object, attributes };
-        }
-        if (equalHtml.test(keyword)) {
-            const equals = keyword.split('=');
-            const key = equals[0].replace(fourSpaces, '');
-            keyword = key;
-            const value = equals[1];
-            object = { ...object, keyword: key, value };
-        }
-        if (!beginningHtml.test(keyword) && !endingHtml.test(keyword)) {
-            if (child) {
-                object = { ...object, begin: `<${keyword}${object.attributes || ''}>`, end: `</${keyword}>` };
-                let lastEntry = array[array.length - 1];
-                if (object.value) {
-                    const str = `${object.begin}${object.value}${object.end}`;
-                    if (lastEntry.children.length > 0) {
-                        for (let i = 0; i < levels; i++) {
-                            lastEntry = lastEntry.children[lastEntry.children - 1];
+                let value = equals[1];
+                value = value[0] === "'" || value[0] === '"' ? value.slice(1, value.length - 2).replace(/\\/g, '') : value;
+                object = { ...object, keyword: key, value };
+            }
+            if (!beginningHtml.test(keyword) && !endingHtml.test(keyword)) {
+                if (child) {
+                    object = { ...object, begin: `<${keyword}${object.attributes || ''}>`, end: `</${keyword}>` };
+                    let lastEntry = array[array.length - 1];
+                    if (object.value) {
+                        const str = `${object.begin}${object.value}${object.end}`;
+                        for (let i = 1; i < levels; i++) {
+                            if (lastEntry.children && lastEntry.children.length > 0) {
+                                lastEntry = lastEntry.children[lastEntry.children.length - 1];
+                            }
                         }
                         lastEntry.children.push({ keyword: str });
                     } else {
-                        lastEntry.children.push({ keyword: str });
+                        const childObject = {};
+                        childObject.begin = object.begin;
+                        childObject.end = object.end;
+                        childObject.children = [];
+                        for (let i = 1; i < levels; i++) {
+                            if (lastEntry.children && lastEntry.children.length > 0) {
+                                lastEntry = lastEntry.children[lastEntry.children.length - 1];
+                            }
+                        }
+                        lastEntry.children.push(childObject);
                     }
+                    child = false;
                 } else {
-                    const childObject = {};
-                    childObject.begin = object.begin;
-                    childObject.end = object.end;
-                    childObject.children = [];
-                    if (lastEntry.children.length > 0) {
-                        for (let i = 0; i < levels; i++) {
-                            lastEntry = lastEntry.children[lastEntry.children - 1];
-                        }
-                        lastEntry.children.push(childObject);
-                    } else {
-                        lastEntry.children.push(childObject);
-                    }
+                    object = { ...object, begin: `<${keyword}${object.attributes || ''}>`, end: `</${keyword}>` };
+                    array.push(object);
                 }
-                child = false;
-            } else {
-                object = { ...object, begin: `<${keyword}${object.attributes || ''}>`, end: `</${keyword}>` };
-                array.push(object);
             }
         }
     }
@@ -99,8 +95,7 @@ const command = (puppyFilePath, options) => {
         return respStr;
     };
 
-    console.log(JSON.stringify(array));
-    console.log(recurseResponse('', array));
+    return recurseResponse('', array);
 };
 
 const documentation = () => {
